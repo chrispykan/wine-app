d17d2ce09bdcfcdf502287762634d2091ddb53f4
chrispykan <christineparisk@gmail.com>
2018-06-25 12:53:32 -0700

    initil commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>I'll soon be a Wine App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
